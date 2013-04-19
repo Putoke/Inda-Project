29d6509155ae6b109cf3ce3e8aa579b7f0a2b9d5
Joakim Edlund <joakim.en.edlund@gmail.com>
2013-04-19 22:21:44 +0200

    Test

--- IndaProject/src/Main.java
+++ IndaProject/src/Main.java
@@ -8,20 +8,21 @@ public class Main extends BasicGame {
 
 	public Main() {
 		super("Super Game");
+		System.out.println("Herro");
 	}
 
 	@Override
 	public void init(GameContainer gc) throws SlickException {
-
+		
 	}
 
 	@Override
 	public void update(GameContainer gc, int delta) throws SlickException {
-
+		
 	}
 
 	public void render(GameContainer gc, Graphics g) throws SlickException {
-
+		
 	}
 
 	public static void main(String[] args) throws SlickException {
