5342af735c22e22604a1b83f0420a1a17f2ed17f
Joakim Edlund <joakim.en.edlund@gmail.com>
2013-04-19 21:55:52 +0200

    Basic slick2D window.

--- IndaProject/src/Main.java
+++ IndaProject/src/Main.java
@@ -1,11 +1,33 @@
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
 
-public class Main {
+public class Main extends BasicGame {
 
-	public Main() {}
-	
-	public static void main(String[] args) {
-		System.out.println("bajs");
-		
-		//bajs
+	public Main() {
+		super("Super Game");
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {
+
+	}
+
+	@Override
+	public void update(GameContainer gc, int delta) throws SlickException {
+
+	}
+
+	public void render(GameContainer gc, Graphics g) throws SlickException {
+
+	}
+
+	public static void main(String[] args) throws SlickException {
+		AppGameContainer app = new AppGameContainer(new Main());
+
+		app.setDisplayMode(800, 600, false);
+		app.start();
 	}
 }
