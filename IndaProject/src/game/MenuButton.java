2a5ddcf4da5c00150607a6d0d71f815d1c448a7c
Fredrik <fredrik.kona@gmail.com>
2013-04-29 15:01:25 +0200

    Herro prease!

---
+++ IndaProject/src/game/MenuButton.java
@@ -0,0 +1,13 @@
+package game;
+
+import org.newdawn.slick.geom.Vector2f;
+
+public class MenuButton extends Entity{
+
+	public MenuButton(String id) {
+		super(id);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
