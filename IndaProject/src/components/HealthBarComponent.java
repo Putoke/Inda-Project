0cf3a963dc78395a939d3f43d6b9812b20ad773a
Putoke <joakim.en.edlund@gmail.com>
2013-05-01 17:03:54 +0200

    Fixed bug with health bar

--- IndaProject/src/components/HealthBarComponent.java
+++ IndaProject/src/components/HealthBarComponent.java
@@ -22,10 +22,11 @@ public class HealthBarComponent extends RenderComponent{
 	public void render(GameContainer gc, StateBasedGame sb, Graphics gr) {
 		Vector2f pos = owner.getPosition();
 		float scale = owner.getScale();
+		float posX = pos.x + owner.getRadius();
 
-		damage.draw(pos.x - owner.getRadius(), pos.y - 30, scale);
-		health.draw(pos.x - owner.getRadius(), pos.y - 30, (float)(owner.getHealth())/(float)(owner.getMaximumHealth()) * health.getWidth(), health.getHeight());
-		bar.draw(pos.x - owner.getRadius(), pos.y - 30, scale);
+		damage.draw(posX - damage.getWidth()/2, pos.y - 30, scale);
+		health.draw(posX - health.getWidth()/2, pos.y - 30, (float)(owner.getHealth())/(float)(owner.getMaximumHealth()) * health.getWidth(), health.getHeight());
+		bar.draw(posX - bar.getWidth()/2, pos.y - 30, scale);
 
 	}
 
