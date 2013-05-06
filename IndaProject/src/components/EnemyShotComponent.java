cf090aa0f278f95ce04619d65a7fb70da9f317b2
Fredrik <fredrik.kona@gmail.com>
2013-05-06 23:58:20 +0200

    enemy shot component

---
+++ IndaProject/src/components/EnemyShotComponent.java
@@ -0,0 +1,42 @@
+package components;
+
+import game.Game;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.geom.Vector2f;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class EnemyShotComponent extends Component{
+
+	private float rotation;
+	private Vector2f position;
+	private float speed;
+	
+	public EnemyShotComponent(String id, float rotation, Vector2f position){
+		this.id = id;
+		this.rotation = rotation + 90;
+		this.position = new Vector2f(position.x, position.y);
+		speed = 2.0f;
+	}
+	
+	public void update(GameContainer gc, StateBasedGame sb, int delta) {
+		float hip = speed * delta;
+
+		position.x += hip * Math.sin(Math.toRadians(rotation - 90));
+		position.y -= hip * Math.cos(Math.toRadians(rotation - 90));
+		owner.setPosition(position);
+		owner.setRotation(rotation);
+		
+		if(leftScreen()){
+			owner.setHealth(0);
+		}
+	}
+
+	private boolean leftScreen(){
+		if(position.x + owner.getRadius()*2 < 0 || position.y + owner.getRadius()*2 < 0 || position.x > Game.app.getWidth() || position.y > Game.app.getHeight()){
+			return true;
+		}
+		return false;
+	}
+
+}
